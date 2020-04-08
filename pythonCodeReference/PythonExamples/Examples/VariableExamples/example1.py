66ea817a0882dd556fff3912c3c0efd1f5a645b4
K Kasi <you@example.com>
2020-04-08 16:30:42 +0530

    first commit

---
+++ pythonCodeReference/PythonExamples/Examples/VariableExamples/example1.py
@@ -0,0 +1,19 @@
+x = 15
+
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+def to_lowercase(input):
+    print(input.lower())
+
+name = "Eric Idle"
+F"{to_lowercase(name)} is funny."
+
+aa = "I ♥ U"  # unicode string starts with “u”
+
+print(aa)
